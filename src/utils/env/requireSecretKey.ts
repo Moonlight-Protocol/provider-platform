354ade5caed4a99a3ed55d3750da1d7766ebb548
Fabricius Zatti <fazzatti@gmail.com>
2025-10-22 08:12:39 -0300

    feat: add requireSecretKey utility to validate Stellar secret keys

---
+++ src/utils/env/requireSecretKey.ts
@@ -0,0 +1,11 @@
+import { type Ed25519SecretKey, StrKey } from "@colibri/core";
+import requireEnv from "./requireEnv.ts";
+
+export const requireSecretKey = (keyName: string): Ed25519SecretKey => {
+  const key = requireEnv(keyName);
+  if (StrKey.isValidEd25519SecretSeed(key) === false) {
+    throw new Error(`Invalid Stellar secret key for ${keyName}`);
+  }
+
+  return key as Ed25519SecretKey;
+};
