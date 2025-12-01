0cc5a0a75dd81b99e482458f7faa4eb4d4c6bd38
Fabricius Zatti <fazzatti@gmail.com>
2025-12-01 15:10:21 -0300

    feat: Replace console logging with logger utility in main application

--- src/main.ts
+++ src/main.ts
@@ -5,18 +5,19 @@ import apiVi from "./http/v1/v1.routes.ts";
 import { appendRequestIdMiddleware } from "./http/middleware/appendRequestId.ts";
 import { appendResponseHeadersMiddleware } from "./http/middleware/appendResponseHeaders.ts";
 
-import { PORT } from "./config/env.ts";
+import { PORT } from "@/config/env.ts";
+import { LOG } from "@/config/logger.ts";
 
 async function bootstrap() {
   const app = new Application();
- 
+
   app.use(globalRateLimitMiddleware);
   app.use(appendRequestIdMiddleware);
   app.use(appendResponseHeadersMiddleware);
   app.use(apiVi.routes());
- 
-  console.log(`🚀 Executer Server running on http://localhost:${PORT}`);
- 
+
+  LOG.info(`🚀 Executer Server running on http://localhost:${PORT}`);
+
   await app.listen({ port: Number(PORT) });
 }
 
