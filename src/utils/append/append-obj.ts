667967f327e0dda0b2c810b3057ac5e42f2019fb
Fabricius Zatti <fazzatti@gmail.com>
2025-12-03 08:23:01 -0300

    refactor: Clean up formatting and remove unnecessary async in appendObjectsTransformerFactory

--- src/utils/append/append-obj.ts
+++ src/utils/append/append-obj.ts
@@ -2,12 +2,12 @@ export const appendObjectsTransformer = <
   K1 extends string,
   O1,
   K2 extends string,
-  O2,
+  O2
 >(
   key1: K1,
   obj1: O1,
   key2: K2,
-  obj2: O2,
+  obj2: O2
 ): { [P in K1 | K2]: P extends K1 ? O1 : O2 } => {
   return {
     [key1]: obj1,
@@ -19,11 +19,11 @@ export const appendObjectsTransformerFactory = <
   K1 extends string,
   O1,
   K2 extends string,
-  O2,
+  O2
 >(
   key1: K1,
   obj1: O1,
-  key2: K2,
+  key2: K2
 ) => {
-  return async (obj2: O2) => appendObjectsTransformer(key1, obj1, key2, obj2);
+  return (obj2: O2) => appendObjectsTransformer(key1, obj1, key2, obj2);
 };
