ccd703b5058be73c9d9077dedcb48d534f55f725
Fabricius Zatti <fazzatti@gmail.com>
2025-12-03 08:08:22 -0300

    refactor: standardize file names and imports

--- src/core/service/auth/service/service-account.ts
+++ src/core/service/auth/service/service-account.ts
@@ -1,10 +1,3 @@
 import { LocalSigner } from "@colibri/core";
 import { PROVIDER_SK } from "@/config/env.ts";
-
-// export const STELLAR_SERVICE_ACCOUNT =
-//   new StellarPlus.Account.DefaultAccountHandler({
-//     networkConfig: NETWORK_CONFIG,
-//     secretKey: PROVIDER_SK,
-//   });
-
 export const PROVIDER_ACCOUNT = LocalSigner.fromSecret(PROVIDER_SK);
--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,8 @@ import { Application } from "@oak/oak";
 
 import { globalRateLimitMiddleware } from "./http/middleware/rate-limit.ts";
 import apiVi from "./http/v1/v1.routes.ts";
-import { appendRequestIdMiddleware } from "./http/middleware/appendRequestId.ts";
-import { appendResponseHeadersMiddleware } from "./http/middleware/appendResponseHeaders.ts";
+import { appendRequestIdMiddleware } from "./http/middleware/append-request-id.ts";
+import { appendResponseHeadersMiddleware } from "./http/middleware/append-response-headers.ts";
 
 import { PORT } from "@/config/env.ts";
 import { LOG } from "@/config/logger.ts";
