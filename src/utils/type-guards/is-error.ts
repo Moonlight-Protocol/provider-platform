eee15967117ad9e1d254080f499c5acb69b8e562
Fabricius Zatti <fazzatti@gmail.com>
2025-12-04 16:33:31 -0300

    fix: correct import path in is-error type guard

--- src/utils/type-guards/is-error.ts
+++ src/utils/type-guards/is-error.ts
@@ -1,4 +1,4 @@
-import { isDefined } from "@/utils//type-guards/is-defined.ts";
+import { isDefined } from "@/utils/type-guards/is-defined.ts";
 
 export function isError(e: unknown): e is Error {
   return isDefined(e) && typeof e === "object" && e instanceof Error;
